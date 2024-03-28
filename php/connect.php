0fa4fa6a3d1674cd71094045275bd35ba66d6b30
ivancotacte <cotactearmenion@gmail.com>
2024-03-29 00:34:48 +0800

    Refactored database connection to use PDO

--- php/connect.php
+++ php/connect.php
@@ -5,10 +5,11 @@ $username = "root";
 $password = "";
 $dbname = "test";
 
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-if (!$conn) {
-    echo "Connection Failed";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
 
 ?>
