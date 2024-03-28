50b2ee06ebce95bf78de670b967d8c6ab7dd132f
ivancotacte <cotactearmenion@gmail.com>
2024-03-28 16:06:23 +0800

    Add SQL schema for test database

---
+++ sql/test.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+CREATE TABLE `tbl_logdata` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `heartRate` varchar(255) NOT NULL,
+    `weight` varchar(255) NOT NULL,
+    `created_at` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tbl_healthmonitor` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `firstName` varchar(255) NOT NULL,
+    `middleName` varchar(255) NOT NULL,
+    `lastName` varchar(255) NOT NULL,
+    `suffix` varchar(255) NOT NULL,
+    `age` varchar(255) NOT NULL,
+    `gender` varchar(255) NOT NULL,
+    `height` varchar(255) NOT NULL,
+    `weight` varchar(255) NOT NULL,
+    `heartRate` varchar(255) NOT NULL,
+    `contactNum` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `created_at` varchar(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
