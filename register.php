044a53e97cf8b806282181a609bb1e6035ee888a
ivancotacte <cotactearmenion@gmail.com>
2024-03-28 16:06:56 +0800

    Refactor function to improve performance

---
+++ register.php
@@ -0,0 +1,123 @@
+<?php
+include 'php/connect.php';
+$msg = "";
+
+if (isset($_POST['submit'])) {
+    $firstName = $_POST['firstName'];
+    $middleName = $_POST['middleName'];
+    $lastName = $_POST['lastName'];
+    $suffix = $_POST['suffix'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $height = $_POST['height'];
+    $contactNum = $_POST['contactNum'];
+    $email = $_POST['email'];
+    
+    $select = "SELECT * FROM tbl_healthmonitor WHERE email='{$email}'";
+    $result = mysqli_query($conn, $select);
+
+    if(mysqli_num_rows($result) > 0){
+        $msg = "<div class='alert alert-danger'>Email already exists.</div>";
+    } else {
+        $current_time = date("Y-m-d H:i:s");
+        $stmt = $conn->prepare("INSERT INTO tbl_healthmonitor (firstName, middleName, lastName, suffix, age, gender, height, contactNum, email, created_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $result = $stmt->execute([$firstName, $middleName, $lastName, $suffix, $age, $gender, $height, $contactNum, $email, $current_time]);
+
+        if ($result) {
+            $msg = "<div class='alert alert-success'>Account successfully created.</div>";
+
+            $newId = mysqli_insert_id($conn);
+            header("Location: checking.php?id=$newId");
+        } else {
+            $msg = "<div class='alert alert-danger'>Failed to create account.</div>";
+        }
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title> Register </title>
+</head>
+<body>
+<div class="container d-flex justify-content-center align-items-center min-vh-100"> 
+    <div class="row border rounded-4 p-3 bg-white shadow box-area">
+      <div class="col-md-5 rounded-4 d-flex justify-content-center align-items-center flex-column left-box"
+        style="background: #030067">
+      </div>
+      <div class="col-md-7 right-box">
+        <div class="row align-items-center">
+          <div class="header-text mb-4">
+            <h2> User Registration Form </h2>
+            <p>To register Health Monitoring, please make sure you meet the following requirements:</p>
+          </div>
+          <div>
+            <?php echo $msg; ?>
+            <form action="" method="post">
+              <label class="form-label">First Name:</label>
+              <div class="input-group mb-2">
+                <input type="text" name="firstName" class="form-control bg-light fs-6" placeholder="Juan" required />
+              </div>
+              <label class="form-label">Middle Name:</label>
+              <div class="input-group mb-2">
+                <input type="text" name="middleName" class="form-control bg-light fs-6" placeholder="Martinez" />
+              </div>
+              <label class="form-label">Last Name:</label>
+              <div class="input-group mb-2">
+                <input type="text" name="lastName" class="form-control bg-light fs-6" placeholder="Dela Cruz" required />
+              </div>
+              <label class="form-label">Suffix:</label>
+              <div class="input-group mb-2">
+                <select name="suffix" id="suffix" class="form-select bg-light fs-6">
+                  <option value="">Suffix</option>
+                  <option value="jr">Jr</option>
+                  <option value="sr">Sr</option>
+                  <option value="II">II</option>
+                  <option value="III">III</option>
+                  <option value="IV">IV</option>
+                </select>
+              </div>
+              <label class="form-label"> Age: </label>
+              <div class="input-group mb-2">
+                <input type="number" name="age" class="form-control bg-light fs-6" required />
+              </div>
+              <label class="form-label"> Gender: </label>
+              <div class="input-group mb-2">
+                  <select name="gender" id="gender" class="form-select bg-light fs-6" required>
+                      <option value="">Gender</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                  </select>
+              </div>
+              <label class="form-label"> Height (CM): </label>
+              <div class="input-group mb-2">
+                <input type="number" name="height" class="form-control bg-light fs-6" required />
+              </div>
+              <label class="form-label"> Contact Number: </label>
+              <div class="input-group mb-2">
+                <input type="number" name="contactNum" class="form-control bg-light fs-6" required />
+              </div>
+              <label class="form-label">Email address:</label>
+              <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control bg-light fs-6" placeholder="example@example.com" required />
+              </div>
+              <div class="input-group mb-2">
+                <button type="submit" name="submit" class="btn btn-lg w-100 fs-6" style="background-color: #030067; color: #ececec;">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
